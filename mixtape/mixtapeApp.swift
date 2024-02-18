61fbb3f32631311020ffad0adcf8702ea5375d8f
BuildTools <olinlj@icloud.com>
2024-02-17 22:45:48 -0500

    Initial Commit

---
+++ mixtape/mixtapeApp.swift
@@ -0,0 +1,17 @@
+//
+//  mixtapeApp.swift
+//  mixtape
+//
+//  Created by Olin Johnson on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct mixtapeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
