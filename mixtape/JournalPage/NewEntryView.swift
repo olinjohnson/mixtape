947fce6dd72f8069897f2160ddda453ca6f2fdae
BuildTools <olinlj@icloud.com>
2024-06-20 12:59:38 -0400

    progress on create mixtape and create entry pages

--- mixtape/JournalPage/NewEntryView.swift
+++ mixtape/JournalPage/NewEntryView.swift
@@ -27,131 +27,126 @@ struct NewEntryView: View {
     
     var body: some View {
         NavigationStack {
-            ZStack {
-                GeometryReader { reader in
-                    ScrollView {
-                        VStack(alignment: .leading) {
-                            GeometryReader { gr in
-                                ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)) {
-                                    Image("ed_sheeran")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fill)
-                                        .frame(width: gr.size.width, height: gr.size.height + max(0, gr.frame(in: .global).origin.y))
-                                        .offset(y: -gr.frame(in: .global).origin.y)
-                                }
+            GeometryReader { reader in
+                ScrollView {
+                    VStack(alignment: .leading) {
+                        GeometryReader { gr in
+                            ZStack(alignment: Alignment(horizontal: .leading, vertical: .top)) {
+                                Image("ed_sheeran")
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fill)
+                                    .frame(width: gr.size.width, height: gr.size.height + max(0, gr.frame(in: .global).origin.y))
+                                    .offset(y: -gr.frame(in: .global).origin.y)
                             }
-                            .frame(height:350)
-                            VStack {
-                                VStack(alignment:.leading) {
-                                    DatePicker("What date?       \(Image(systemName: "arrow.right"))", selection: $inputDate, displayedComponents: .date)//.labelsHidden()
-                                        .padding([.bottom, .top])
-                                        .font(.title3)
-                                    //.bold()
-                                    Divider()
-                                        .padding([.top, .bottom])
-                                    TextField("Put your title here...", text: $inputTitle, axis:.vertical)
-                                        .font(.title)
+                            HStack {
+                                VStack(alignment:.leading){
+                                    Text("Click text to edit")
+                                        .font(.title2)
                                         .bold()
-                                        .focused($textFieldFocus, equals:.title)
-                                    TextField("What would you like to write about today?", text: $inputBody, axis:.vertical)
-                                        .focused($textFieldFocus, equals:.body)
+                                    Text("Bottom buttons to create or cancel")
+                                        .font(.caption)
+                                        .foregroundStyle(.black.opacity(0.6))
                                 }
-                                .padding([.leading, .trailing])
-                                
                                 Spacer()
                                 
-                                // Bottom Buttons
-                                HStack {
-                                    Button(action:{showingCancelAlert = true}) {
-                                        Text("Cancel")
+                                if textFieldFocus == .body || textFieldFocus == .title{
+                                    Button(action:{
+                                        textFieldFocus = nil
+                                    }) {
+                                        Text("Done")
+                                    }
+                                    .onAppear {
+                                        if textFieldFocus != .title {
+                                            keyboardOffsetAmt = 210
+                                        }
+                                    }
+                                }else{
+                                    Button(action:{
+                                        textFieldFocus = .body
+                                    }) {
+                                        Image(systemName: "pencil")
+                                            .font(.largeTitle)
+                                            .foregroundColor(.blue)
+                                    }
+                                    .onAppear {
+                                        keyboardOffsetAmt = 0
+                                    }
+                                }
+                            }
+                            .padding(20)
+                            .frame(maxWidth:.infinity)
+                            .background(.white.opacity(0.9))
+                            .cornerRadius(12)
+                            .shadow(color: .black.opacity(0.2), radius: 10, x: 2, y: 2)
+                            .offset(y: -gr.frame(in: .global).origin.y + 60)
+                            .padding([.leading, .trailing])
+                        }
+                        .frame(height:350)
+                        VStack {
+                            VStack(alignment:.leading) {
+                                DatePicker("What date?       \(Image(systemName: "arrow.right"))", selection: $inputDate, displayedComponents: .date)//.labelsHidden()
+                                    .padding([.bottom, .top])
+                                    .font(.title3)
+                                //.bold()
+                                Divider()
+                                    .padding([.top, .bottom])
+                                TextField("Put your title here...", text: $inputTitle, axis:.vertical)
+                                    .font(.title)
+                                    .bold()
+                                    .focused($textFieldFocus, equals:.title)
+                                TextField("What would you like to write about today?", text: $inputBody, axis:.vertical)
+                                    .focused($textFieldFocus, equals:.body)
+                            }
+                            .padding([.leading, .trailing, .bottom])
+                            
+                            Spacer()
+                            
+                            // Bottom Buttons
+                            HStack {
+                                Button(action:{showingCancelAlert = true}) {
+                                    Text("Cancel")
+                                        .padding()
+                                        .frame(maxWidth:.infinity)
+                                        .background(.red)
+                                        .cornerRadius(10)
+                                        .foregroundColor(.white)
+                                        .font(.title3)
+                                        .bold()
+                                }
+                                .shadow(color: .black.opacity(0.2), radius: 8, x: 1, y: 1)
+                                .alert(isPresented:$showingCancelAlert) {
+                                    Alert(
+                                        title:Text("Are you sure you want to cancel?"),
+                                        message:Text("Your changes will not be saved."),
+                                        primaryButton: .destructive(Text("I'm sure")) {dismiss()},
+                                        secondaryButton: .cancel(Text("No, go back"))
+                                    )
+                                }
+                                
+                                Button(action:{}) {
+                                    NavigationLink(destination:JournalView().toolbar(.visible, for: .tabBar)) {
+                                        Text("Create")
                                             .padding()
                                             .frame(maxWidth:.infinity)
-                                            .background(.red)
+                                            .background(.green)
                                             .cornerRadius(10)
                                             .foregroundColor(.white)
                                             .font(.title3)
                                             .bold()
                                     }
-                                    .shadow(color: .black.opacity(0.2), radius: 8, x: 1, y: 1)
-                                    .alert(isPresented:$showingCancelAlert) {
-                                        Alert(
-                                            title:Text("Are you sure you want to cancel?"),
-                                            message:Text("Your changes will not be saved."),
-                                            primaryButton: .destructive(Text("I'm sure")) {dismiss()},
-                                            secondaryButton: .cancel(Text("No, go back"))
-                                        )
-                                    }
-                                    
-                                    Button(action:{}) {
-                                        NavigationLink(destination:JournalView().toolbar(.visible, for: .tabBar)) {
-                                            Text("Create")
-                                                .padding()
-                                                .frame(maxWidth:.infinity)
-                                                .background(.green)
-                                                .cornerRadius(10)
-                                                .foregroundColor(.white)
-                                                .font(.title3)
-                                                .bold()
-                                        }
-                                    }
-                                    .shadow(color: .black.opacity(0.2), radius: 8, x: 1, y: 1)
                                 }
+                                .shadow(color: .black.opacity(0.2), radius: 8, x: 1, y: 1)
                             }
-                            .padding()
-                            .background(.white)
-                            // Cover image box
-                            // add music
                         }
-                        .offset(y:CGFloat(-keyboardOffsetAmt))
-                        .frame(minHeight: reader.size.height)
+                        .padding()
+                        .background(.white)
+                        // Cover image box
+                        // add music
                     }
-                    //.edgesIgnoringSafeArea(.all)
+                    .offset(y:CGFloat(-keyboardOffsetAmt))
+                    .frame(minHeight: reader.size.height)
                 }
-
-                VStack {
-                    HStack {
-                        VStack(alignment:.leading){
-                            Text("Click text to edit")
-                                .font(.title2)
-                                .bold()
-                            Text("Bottom buttons to create or cancel")
-                                .font(.caption)
-                                .foregroundStyle(.black.opacity(0.6))
-                        }
-                        Spacer()
-                        
-                        if textFieldFocus == .body || textFieldFocus == .title{
-                            Button(action:{
-                                textFieldFocus = nil
-                            }) {
-                                Text("Done")
-                            }
-                            .onAppear {
-                                if textFieldFocus != .title {
-                                    keyboardOffsetAmt = 210
-                                }
-                            }
-                        }else{
-                            Button(action:{
-                                textFieldFocus = .body
-                            }) {
-                                Image(systemName: "pencil")
-                                    .font(.largeTitle)
-                                    .foregroundColor(.blue)
-                            }
-                            .onAppear {
-                                keyboardOffsetAmt = 0
-                            }
-                        }
-                    }
-                    .padding(20)
-                    .frame(maxWidth:.infinity)
-                    .background(.white.opacity(0.9))
-                    .cornerRadius(12)
-                    .shadow(color: .black.opacity(0.2), radius: 10, x: 2, y: 2)
-                    Spacer()
-                }
-                .padding()
+                //.edgesIgnoringSafeArea(.all)
             }
         }
         .navigationBarBackButtonHidden(true)
--- mixtape/partials/ImagePickerView.swift
+++ mixtape/partials/ImagePickerView.swift
@@ -1,5 +1,5 @@
 //
-//  InstructionBarView.swift
+//  ImagePickerView.swift
 //  mixtape
 //
 //  Created by Olin Johnson on 6/20/24.
@@ -7,12 +7,12 @@
 
 import SwiftUI
 
-struct InstructionBarView: View {
+struct ImagePickerView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    InstructionBarView()
+    ImagePickerView()
 }
